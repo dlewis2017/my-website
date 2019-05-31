4cbaa0c3c56f23c74203bef617afd47c0c6f0747
David <dlewfaith@gmail.com>
2019-05-30 22:50:57 -0400

    Two profiles initial set up

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
+    let profile = 0;
 
-    // Uncheck checkbox when nav item clicked (hide nav)
+    // active class to hide card side
+    $('.header__logo-box').click(function() {
+        if (!profile) {
+            $('#card__side--back').removeClass('hide');
+            $('#card__side--front').addClass('hide');
+            profile = !profile;
+        } else {
+            $('#card__side--front').removeClass('hide');
+            $('#card__side--back').addClass('hide');
+            profile = !profile;
+        }
+    });
+
     $('.navigation__item').click(function() {
-        $('.navigation__checkbox').prop('checked', false);
+        $("#navi-toggle").prop("checked", false);
     });
 
 });
