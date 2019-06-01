ed510fdeebfc8a90e6f9f36b562f5de773c9d33b
dlewis2017 <dlewfaith@gmail.com>
2019-06-01 02:28:33 -0400

    minor changes

--- js/main.js
+++ js/main.js
@@ -2,29 +2,7 @@ $(document).ready(function() {
     let profile = 0;
 
     // active class to hide card side
-    $('.header__logo-box').click(function() {
-        if (!profile) {
-            $('#card__side--back').removeClass('hide');
-            $('#card__side--front').addClass('hide');
-            profile = !profile;
-        } else {
-            $('#card__side--front').removeClass('hide');
-            $('#card__side--back').addClass('hide');
-            profile = !profile;
-        }
-    });
-    $('#otherProfile').click(function() {
-        if (!profile) {
-            $('#card__side--back').removeClass('hide');
-            $('#card__side--front').addClass('hide');
-            profile = !profile;
-        } else {
-            $('#card__side--front').removeClass('hide');
-            $('#card__side--back').addClass('hide');
-            profile = !profile;
-        }
-    });
-    $('#otherProfile--dev').click(function() {
+    $('.header__logo-box, #otherProfile, #otherProfile--dev').click(function() {
         if (!profile) {
             $('#card__side--back').removeClass('hide');
             $('#card__side--front').addClass('hide');
